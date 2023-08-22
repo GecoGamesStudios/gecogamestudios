83651925c23fa4cde3513d9271d2d3b3edd1a38e
GecoGamesStudios <140628063+GecoGamesStudios@users.noreply.github.com>
2023-08-22 23:41:53 +0200

    Delete Gecogames directory

--- Gecogames/community/community.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.navigation-wrapper');
-
-hamburgerMenu.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
