77765a0829f0b26466771f2091def20f4dfabe67
JosephTy7 <114694896+JosephTy7@users.noreply.github.com>
2023-09-16 16:35:39 +0200

    Add files via upload
    just update the css

---
+++ extras.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Geco Games Studios</title>
+    <link rel="stylesheet" href="../css/extras.css">
+    <link rel="stylesheet "type="text/css" href="../css/navigations.css">  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+<header>
+        <div class="logo">
+            <a href="../index.php">
+                <img src="../assets/logos/black.png" alt="geco-games-studios">
+            </a>
+        </div>
+        <!--three line hambuger menu-->
+        <div class="hamburger-container" id="hamburger">
+            <div class="hamburger-menu">
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+            
+        </div>
+        <!--Navigation links-->
+        <div class="navigation-wrapper" id="nav-menue">
+            <nav class="navigation-menu">
+                <a href="../index.php">Home</a>
+                <a href="./games.php">Games</a>
+                <a href="./community.php">Community</a>
+                <a href="./contactus.php">Contact Us</a>  
+                <a href="./support.php">Support</a>
+                <a href="./developus.php">Developers</a></li>
+                <div class="dropdown">
+  <a href="./pages/extras.php" class="drop-link">Extras <i class="fas fa-caret-down"></i></a>
+    <div class="dropdown-content">
+      <a href="https://www.nerdotaku.org/">NerdOtaku</a>
+      <a href="https://www.nerdotaku.org/project/lscon-2023/">LScon</a>
+    </div>
+  </div>
+</div>
+                <div class="user-login">
+                    <a href="./pages/userforms.php">Log In</a>
+                    <a href="./pages/userforms.php">Sign Up</a>
+                </div>
+            </nav>
+            <div class="closer">
+            <i class="fa-solid fa-xmark fa-2xl" id="closer"></i>
+            </div>
+        </div>
+      
+    </header>
+    <div class="main-content">
+        <div class="card">
+            <div class="left-column">
+                <h2>Get Your Tickets</h2>
+                <div class="event-description">
+                    <p class="event-text">Welcome to LScon, the ultimate gathering for anime, gaming, and comics enthusiasts! Join us for a weekend of immersive experiences, where you can dive into the vibrant worlds of your favorite anime, discover the latest in gaming innovations, and explore the fascinating realm of comics.</p>
+                    <a href="#" class="register-button">Register Now</a>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="right-column">
+                <img src="../assets/images/IMG_20230707_112726_151.jpeg.jpg" alt="Gaming Image">
+            </div>
+        </div>
+    </div>
+    
+    <div class="main-content">
+        <div class="card">
+            <div class="card special-card"> 
+                <div class="right-column">
+                    <img src="../assets/images/Final_Original_Honor_white_v2 (1).png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="left-column">
+                <h2>Join Us At NerdOtaku</h2>
+                <div class="event-description">
+                    <p class="event-text"> NerdOtaku promises thrilling panels, epic cosplay competitions, and a chance to connect with fellow fans who share your passion. Get ready to embark on an unforgettable journey into the heart of pop culture!</p>
+    <a href="#" class="register-button">VIST US</a>
+                </div>
+            </div>
+        </div>   
+    </div>
+    <section>
+    <section id="news">
+        <div class="container">
+            <h2>upcoming-Events</h2>
+            <div class="news-article">
+                <i class="fas fa-calendar"></i>
+                <h3>NerdOtaku</h3>
+                <p>Attendees can immerse themselves in a world of fantasy and become part of a larger tapestry of fandom!.</p>
+                <a href="https://www.nerdotaku.org/">Learn More</a>
+            </div>
+            <div class="news-article">
+                <i class="fas fa-calendar"></i>
+                <h3>LScon</h3>
+                <p>LsCon, also known as Lusaka Comic Con by Nerd Otaku, stands as a grand celebration, a vibrant gathering that cherishes and nurtures the extraordinary talents residing within our community. .</p>
+                <a href="https://www.nerdotaku.org/project/lscon-2023/">Register Now</a>
+            </div>
+            <div class="news-article">
+                <i class="fas fa-calendar"></i>
+                <h3>Game Expo 2023</h3>
+                <p>Experience the future of gaming at Game Expo 2023. Play upcoming titles, meet game creators, and immerse yourself in the gaming world.</p>
+                <a href="#">Get Tickets</a>
+            </div>
+    
+        </div>
+    </section>
+    
+    
+      <section id="news">
+        <div class="container">
+            <h2>Latest News</h2>
+            <div class="news-article">
+                <i class="fas fa-newspaper"></i>
+                <h3>New Game Announcements</h3>
+                <p>Discover our latest game announcements and get ready for exciting adventures in the world of gaming.</p>
+                <a href="#">Read More</a>
+            </div>
+            <div class="news-article">
+                <i class="fas fa-gamepad"></i>
+                <h3>Gameplay Tips and Tricks</h3>
+                <p>Learn expert tips and tricks to improve your gaming skills and conquer new challenges.</p>
+                <a href="#">Read More</a>
+            </div>
+            <div class="news-article">
+                <i class="fas fa-users"></i>
+                <h3>Community Spotlights</h3>
+                <p>Explore the amazing creations and achievements of our gaming community members.</p>
+                <a href="#">Read More</a>
+            </div>
+            <div class="news-article">
+                <i class="fas fa-trophy"></i>
+                <h3>Game Tournaments</h3>
+                <p>Compete in thrilling tournaments and win fantastic prizes. Are you up for the challenge?</p>
+                <a href="#">Read More</a>
+            </div>
+            <!-- Add more news articles as needed -->
+        </div>
+    </section>
+    <section>
+        <h2>Game Reviews</h2>
+        <div class="game-review">
+            <i class="fas fa-star"></i>
+            <h3>Inshimu 2</h3>
+            <p>Rated 4.5/5 stars</p>
+            <p>Join the endless shooter adventure with Inshimu 2. Defend your hive from invading creatures in this action-packed game.</p>
+            <a href="#">Read Review</a>
+        </div>
+        
+        <div class="game-review">
+            <i class="fas fa-star"></i>
+            <h3>Letter Loop</h3>
+            <p>Rated 4.5/5 stars</p>
+            <p>Challenge your word skills with Letter Loop. Create words and solve puzzles as you progress through levels in this word game.</p>
+            <a href="#">Read Review</a>
+        </div>
+        
+        <div class="game-review">
+            <i class="fas fa-star"></i>
+            <h3>Our Last Prayer</h3>
+            <p>Rated 4.5/5 stars</p>
+            <p>Enter a 2D multiplayer world with Our Last Prayer. Team up with friends and engage in epic battles in this action-packed game.</p>
+            <a href="#">Read Review</a>
+        </div>
+    </section>
