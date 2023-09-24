ceb048732b08729a921bfc01ff36558962eaccc0
Elijah Kalosa <124288198+kalosa0770@users.noreply.github.com>
2023-09-24 16:12:48 +0100

    Add files via upload

---
+++ geco-updated/menu-bar.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.menu');
+
+    // Add a click event listener to the menu icon
+    menuIcon.addEventListener('click', function () {
+        // Toggle the 'active' class on the menu to show/hide it
+        menu.classList.toggle('active');
+    });
+});
